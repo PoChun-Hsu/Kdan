9108ce83fda2084ce32ab5a84ecb02609ca11e7b
PoChun-Hsu <150916046+PoChun-Hsu@users.noreply.github.com>
2023-11-21 09:30:38 +0800

    Update bigquery_operations.py
    Code with seperate file: biguqery_utils

--- bigquery_operations.py
+++ bigquery_operations.py
@@ -1,27 +1,50 @@
 from google.cloud import bigquery
-import pandas as pd
-from datetime import datetime
+from google.cloud.bigquery import Table
+from google.cloud.exceptions import NotFound
 
-# 創建 BigQuery 客戶端
-project_id = 'starry-center-405211'  # 將 '您的專案 ID' 替換為您的 Google Cloud 專案 ID
-client = bigquery.Client(project=project_id)
+def manage_bigquery_tables(client, source_table_id, backup_table_id):
+    try:
+        # Step 1: 刪除 backup 表格（如果存在）
+        client.delete_table(backup_table_id, not_found_ok=True)
 
-def get_latest_date_for_stock(client, table_id, stock_no):
-    """
-    獲取特定股票代碼的最新日期。
-    """
-    query = f"""
-        SELECT MAX(Date) as latest_date
-        FROM `{table_id}`
-        WHERE Stock_Code = '{stock_no}'
-    """
-    query_job = client.query(query)
-    results = query_job.result()
-    for row in results:
-        return row.latest_date
+        # Step 2: 重命名原表格為 backup
+        client.copy_table(source_table_id, backup_table_id)
+        client.delete_table(source_table_id, not_found_ok=True)
+
+        print(f"表格 {source_table_id} 已重命名為 {backup_table_id}")
+        return (None, 200)
+    except Exception as e:
+        print(f"BigQuery 表格操作錯誤: {e}")
+        return (None, 500)
+
+def table_exists(client, table_id):
+    try:
+        client.get_table(table_id)  # 使用 get_table 嘗試獲取表格
+        return (True,200)
+    except NotFound:
+        return (False,500)  # 如果表格不存在，返回 False
 
 
-def truncate_and_load_table(client, df, table_id, stock_no):
-    job = client.load_table_from_dataframe(df, table_id)
-    job.result()
-    print(f"新數據已寫入表格 {table_id}。")
+def create_temp_table(client, source_table_id, temp_table_id):
+    try:
+        # 檢查臨時表格是否存在，如果存在則刪除
+        if table_exists(client, temp_table_id):
+            client.delete_table(temp_table_id)
+            print(f"已存在的表格 {temp_table_id} 已刪除。")
+
+        # 獲取原表格的結構
+        source_table = client.get_table(source_table_id)
+
+        # 創建一個新的 Table 實例，並將結構設定為源表格的結構
+        temp_table = Table(temp_table_id, schema=source_table.schema)
+        
+        # 設定分簇字段
+        temp_table.clustering_fields = ["Date", "Stock_Code"]
+        # 創建表格
+        client.create_table(temp_table)
+        print(f"表格 {temp_table_id} 已創建。")
+
+        return (None, 200)
+    except Exception as e:
+        print(f"創建表格時出錯: {e}")
+        return (None, 500)
